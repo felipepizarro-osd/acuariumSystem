4421b1f5a0ba36a394dc11befd1b00b9a4c2b0b1
Felipe Pizarro <66143232+felipepizarro-osd@users.noreply.github.com>
2023-07-10 20:02:23 -0400

    cross policy methods

--- server.js
+++ server.js
@@ -81,7 +81,9 @@ board.on("ready", function() {
 const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // permite solicitudes de cualquier origen
+  res.header('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // permite solicitudes de cualquier origen
   next()
 })
 app.get('/api/temperature', async (req, res) => {
